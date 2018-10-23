30b30cc3569ba71653e0ae5440bf3f7c6c8483f0
Tyler Long <tyler4long@gmail.com>
2018-10-23 17:35:45 +0800

    Multiple bot user tokens

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+const fs = require('fs')
+
 const dotenv = require('dotenv')
 dotenv.config()
 
 const express = require('express')
 const app = express()
 
+const SubX = require('subx')
+const store = SubX.create({
+  botTokens: []
+})
+const botTokens = require('./bot-tokens.json')
+store.botTokens = botTokens
+SubX.autoRun(store, () => {
+  fs.writeFileSync('./bot-tokens.json', JSON.stringify(store.botTokens, null, 2))
+})
+
 const RingCentral = require('ringcentral-js-concise').default
 const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER)
 
 app.get('/botoauth', async (req, res) => {
-  console.log(req.query.code)
   const code = req.query.code
   try {
     await rc.authorize({ code, redirectUri: process.env.RINGCENTRAL_REDIRECT_URI })
@@ -16,6 +27,7 @@ app.get('/botoauth', async (req, res) => {
     console.log(JSON.stringify(e.response.data, null, 2))
   }
   console.log(rc.token())
+  store.botTokens.push(rc.token())
   res.status(400)
   res.send('hello world')
 })
