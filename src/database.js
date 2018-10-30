d9670da589ffa039cfbeba558a961ff817e28af5
Tyler Long <tyler4long@gmail.com>
2018-10-30 14:51:06 +0800

    Refactor code

--- src/database.js
+++ src/database.js
@@ -1,6 +1,11 @@
 import path from 'path'
 import fs from 'fs'
 import AWS from 'aws-sdk'
+import { promisify } from 'util'
+
+const fsAccess = promisify(fs.access)
+const fsWriteFile = promisify(fs.writeFile)
+const fsReadFile = promisify(fs.readFile)
 
 const s3 = new AWS.S3()
 
@@ -19,58 +24,31 @@ export class FileDatabase extends Database {
   constructor (options) {
     super(options)
     if (!this.file) {
-      this.file = 'database.json'
+      throw new TypeError(`Please specify valid file like this: \`new FileDatabase({ file: '...' })\``)
     }
-    this.databaseUri = path.join(__dirname, this.file)
+    if (path.isAbsolute(this.file)) {
+      this.databaseUri = this.file
+    } else {
+      this.databaseUri = path.join(__dirname, this.file)
+    }
   }
 
   async ensure () {
-    const exists = await new Promise((resolve, reject) => {
-      fs.access(this.databaseUri, fs.constants.F_OK, e => {
-        if (e) {
-          resolve(false)
-        } else {
-          resolve(true)
-        }
-      })
-    })
-    if (!exists) {
-      return new Promise((resolve, reject) => {
-        fs.writeFile(this.databaseUri, '{}', e => {
-          if (e) {
-            reject(e)
-          } else {
-            resolve()
-          }
-        })
-      })
+    try {
+      await fsAccess(this.databaseUri, fs.constants.F_OK)
+    } catch (e) {
+      await fsWriteFile(this.databaseUri, '{}')
     }
   }
 
   async read () {
     await this.ensure()
-    const data = await new Promise((resolve, reject) => {
-      fs.readFile(this.databaseUri, 'utf8', (e, data) => {
-        if (e) {
-          reject(e)
-        } else {
-          resolve(data)
-        }
-      })
-    })
+    const data = await fsReadFile(this.databaseUri, 'utf8')
     return JSON.parse(data)
   }
 
-  write (json) {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(this.databaseUri, JSON.stringify(json, null, 2), e => {
-        if (e) {
-          reject(e)
-        } else {
-          resolve()
-        }
-      })
-    })
+  async write (json) {
+    await fsWriteFile(this.databaseUri, JSON.stringify(json, null, 2))
   }
 }
 
@@ -78,7 +56,7 @@ export class S3Database extends Database {
   constructor (options) {
     super(options)
     if (!this.bucket || !this.key) {
-      throw new TypeError('Please specify valid bucket and key for S3 Database')
+      throw new TypeError(`Please specify valid bucket and key like this: \`new S3Database({ bucket: '...', key: '...' })\``)
     }
     this.options = { Bucket: this.bucket, Key: this.key }
   }
@@ -115,6 +93,6 @@ export class S3Database extends Database {
 
   async write (json) {
     await this.ensureBucket()
-    return s3.putObject({ ...this.options, Body: JSON.stringify(json, null, 2) }).promise()
+    await s3.putObject({ ...this.options, Body: JSON.stringify(json, null, 2) }).promise()
   }
 }
